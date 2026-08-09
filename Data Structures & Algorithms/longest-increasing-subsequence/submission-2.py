a6416e36dc2e9b72eb862f7c04d413226cdb7af6
phongtnguyen2006 <124423542+phongtnguyen2006@users.noreply.github.com>
2026-08-09 15:33:00 +0000

    Add: longest-increasing-subsequence - submission-2

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        """
+        iterate each position
+        dp array to track longest seq at an index
+        """
+
+        if len(nums) == 1:
+            return 1
+        dp = [1] * len(nums) 
+        sol = 0
+
+        for i in range(1, len(nums)):
+            curr_max = 0
+            for j in range(i - 1, -1, -1):
+                if nums[j] < nums[i]:
+                    curr_max = max(curr_max, dp[j])
+            
+            dp[i] += curr_max
+            sol = max(sol, dp[i])
+
+        print(dp)
+        return sol
